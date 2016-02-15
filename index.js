a2b30b5f9163b132755d6df14b4c7475691ff23b
Henri Normak <henri.normak@gmail.com>
2016-02-15 15:44:05 +0200

    Export auth module

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const config = require('./lib/configuration');
+const auth = require('./lib/authentication');
 const path = require('path');
 
 module.exports = {
+    authentication: auth,
     configuration: config(path.resolve(process.cwd(), 'config'))
 };
