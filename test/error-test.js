40c57e869664006eab8ace97ee8b51afc2236af9
Kairi Schotter <kairi@teslio.com>
2016-02-16 15:59:53 +0200

    Option to tag errors

--- test/error-test.js
+++ test/error-test.js
@@ -5,7 +5,7 @@ const raygunStub = {
         'Client': function() {
             let init = function() {
                 return {
-                    send: function(err, data, cb) {
+                    send: function(err, data, cb, tags) {
                         cb();
                     }
                 };
@@ -17,14 +17,14 @@ const raygunStub = {
 
 const proxyquire = require('proxyquire');
 const tape = require('tape');
-const Error = require('../lib/error');
+const Error = require('../lib/error')(['test tag']);
 const Reporter = proxyquire('../lib/error/reporter.js', raygunStub);
 
 tape.test('test Raygun reporting', function(t) {
     let code = 401;
     let message = 'serious error happened';
 
-    Reporter.report(new Error.error(code, message)).then(function(err) {
+    Error.report(new Error.error(code, message)).then(function(err) {
             t.equal(err.message, code + ': ' + message, 'Error is correct');
             t.end();
         }
