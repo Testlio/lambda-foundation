ee67a6e1d66a1f91e53fd7bf397094a37c356e01
Kairi Schotter <kairi@teslio.com>
2016-02-16 22:01:18 +0200

    Separate Raygun into module and add tests

--- test/error-test.js
+++ test/error-test.js
@@ -1,33 +1,56 @@
 'use strict';
 
-const raygunStub = {
-    'raygun': {
-        'Client': function() {
-            let init = function() {
-                return {
-                    send: function(err, data, cb, tags) {
-                        cb();
-                    }
-                };
-            };
-            return {init};
-        }
-    }
-}
-
 const proxyquire = require('proxyquire');
 const tape = require('tape');
-const Error = require('../lib/error')(['test tag']);
-const Reporter = proxyquire('../lib/error/reporter.js', raygunStub);
+const Error = require('../lib/error')(['TEST']);
+const Reporter = require('../lib/error/reporter.js');
+const Raygun = require('../lib/error/raygun.js');
+const sinon = require('sinon');
 
-tape.test('test Raygun reporting', function(t) {
+tape.test('Error reporting resolves to error', function(t) {
     let code = 401;
-    let message = 'serious error happened';
+    let message = 'A serious error happened';
+
+    let error = new Error.error(code, message);
+
+    Error.report(error).then(function(err) {
+            t.same(err, error, 'Error is resolved');
+            t.end();
+        }
+    );
+});
+
+tape.test('Error sent to Raygun', function(t) {
+    let code = 401;
+    let message = 'A serious error happened';
     let extra = {'id': 123};
     let request = {'request':'caused error'}
 
-    Error.report(new Error.error(code, message, extra, request)).then(function(err) {
-            t.equal(err.message, code + ': ' + message, 'Error is correct');
+    let expectedError = new Error.error(code, message, extra, request);
+
+    Raygun.send = function(actualError, extra, cb, request, tags) {
+        t.same(expectedError, actualError, 'Error reported to Raygun');
+        t.equal(expectedError.extra, extra, 'Extras reported to Raygun');
+        t.equal(expectedError.request, request, 'Request reported to Raygun');
+        cb();
+    };
+
+    Error.report(expectedError).then(function(err) {
+            t.end();
+        }
+    );
+});
+
+tape.test('Error reporting failure resolves to error', function(t) {
+    let code = 401;
+    let message = 'A serious error happened';
+
+    let expectedError = new Error.error(code, message);
+
+    Raygun.send = {};
+
+    Error.report(expectedError).then(function(actualError) {
+            t.same(expectedError, actualError, 'Error reported to Raygun');
             t.end();
         }
     );
