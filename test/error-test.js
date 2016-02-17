567b494f8039dfeaf29266f1dc30a5da58beabea
kaarelp <kaarel.purde@testlio.com>
2016-02-17 18:15:30 +0200

    fixed linter errors except for 2 cases: i didnt know how to add specific exceptions to console.log() calls

--- test/error-test.js
+++ test/error-test.js
@@ -5,50 +5,47 @@ const Error = require('../lib/error')(['TEST']);
 const Raygun = require('../lib/error/raygun.js');
 
 tape.test('Error reporting resolves to error', function(t) {
-    let code = 401;
-    let message = 'A serious error happened';
+    const code = 401;
+    const message = 'A serious error happened';
 
-    let error = new Error.error(code, message);
+    const error = new Error.error(code, message);
 
     Error.report(error).then(function(err) {
-            t.same(err, error, 'Error is resolved');
-            t.end();
-        }
-    );
+        t.same(err, error, 'Error is resolved');
+        t.end();
+    });
 });
 
 tape.test('Error sent to Raygun', function(t) {
-    let code = 401;
-    let message = 'A serious error happened';
-    let extra = {'id': 123};
-    let request = {'request':'caused error'}
+    const expectedCode = 401;
+    const expectedMessage = 'A serious error happened';
+    const expectedExtra = {'id': 123};
+    const expectedRequest = {'request':'caused error'};
 
-    let expectedError = new Error.error(code, message, extra, request);
+    const expectedError = new Error.error(expectedCode, expectedMessage, expectedExtra, expectedRequest);
 
-    Raygun.send = function(actualError, extra, cb, request, tags) {
+    Raygun.send = function(actualError, actualExtra, cb, actualRequest) {
         t.same(expectedError, actualError, 'Error reported to Raygun');
-        t.equal(expectedError.extra, extra, 'Extras reported to Raygun');
-        t.equal(expectedError.request, request, 'Request reported to Raygun');
+        t.equal(expectedError.extra, actualExtra, 'Extras reported to Raygun');
+        t.equal(expectedError.request, actualRequest, 'Request reported to Raygun');
         cb();
     };
 
-    Error.report(expectedError).then(function(err) {
-            t.end();
-        }
-    );
+    Error.report(expectedError).then(function() {
+        t.end();
+    });
 });
 
 tape.test('Error reporting failure resolves to error', function(t) {
-    let code = 401;
-    let message = 'A serious error happened';
+    const expectedCode = 401;
+    const expectedMessage = 'A serious error happened';
 
-    let expectedError = new Error.error(code, message);
+    const expectedError = new Error.error(expectedCode, expectedMessage);
 
     Raygun.send = {};
 
     Error.report(expectedError).then(function(actualError) {
-            t.same(expectedError, actualError, 'Error reported to Raygun');
-            t.end();
-        }
-    );
+        t.same(expectedError, actualError, 'Error reported to Raygun');
+        t.end();
+    });
 });
