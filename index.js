2c42e5f0c57e48f5c912a98c3bb08a13d86351c0
Henri Normak <henri.normak@gmail.com>
2016-02-17 21:30:21 +0200

    Include error in the top-level index

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const config = require('./lib/configuration');
 const auth = require('./lib/authentication');
+const error = require('./lib/error');
 const path = require('path');
 
 module.exports = {
     authentication: auth,
-    configuration: config(path.resolve(process.cwd(), 'config'))
+    configuration: config(path.resolve(process.cwd(), 'config')),
+    error: error
 };
