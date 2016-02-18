f0131d0fe526d535d1455cf164ed50487f89f07f
Henri Normak <henri.normak@gmail.com>
2016-02-18 14:27:21 +0200

    Make discovery work similarly to configuration
    Allows defining the discovery file path in env or otherwise the CWD is used.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const _ = require('lodash');
 
-const config = require('./lib/configuration')(path.resolve(process.cwd(), 'config'));
+const config = require('./lib/configuration');
 const auth = require('./lib/authentication');
 const discovery = require('./lib/discovery');
 const error = require('./lib/error');
@@ -11,7 +11,7 @@ const error = require('./lib/error');
 module.exports = function() {
     // Create/read in a base HREF and resolve resources
     const baseURL = _.trimEnd(_.get(config, 'url.base', ''), '/');
-    const resources = discovery(baseURL, path.resolve(process.cwd(), 'discovery.json'));
+    const resources = discovery(baseURL);
 
     return {
         authentication: auth,
