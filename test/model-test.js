0939ef0a40d5fa20fbd7e2e56e34080d8460087a
Kairi Schotter <kairi@teslio.com>
2016-03-01 13:48:29 +0200

    First commit, basic model.

---
+++ test/model-test.js
@@ -0,0 +1,17 @@
+const joi = require('joi');
+const tape = require('tape');
+const Model = require('../lib/model');
+
+/*eslint-disable no-console */
+tape.test('Should create model', function(t) {
+    const testModel = new Model('Test', {
+        hashKey : 'guid',
+        schema : {
+            guid : joi.string().guid(),
+            property: joi.number()
+        }
+    }, {region: 'us-east-1'});
+
+    t.equal(testModel.table.tableName, 'test', 'Correct table name');
+    t.end();
+});
