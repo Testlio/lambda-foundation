a81f499a1436d1b436ce271a476604a4d03c7612
Henri Normak <henri.normak@gmail.com>
2016-03-01 17:47:15 +0200

    Flatten main and configuration exports

--- index.js
+++ index.js
@@ -2,20 +2,18 @@
 
 const _ = require('lodash');
 
-const config = require('./lib/configuration');
+const configuration = require('./lib/configuration');
 const auth = require('./lib/authentication');
 const discovery = require('./lib/discovery');
 const error = require('./lib/error');
 
-module.exports = function() {
-    // Create/read in a base HREF and resolve resources
-    const baseURL = _.trimEnd(_.get(config, 'url.base', ''), '/');
-    const resources = discovery(baseURL);
+// Create/read in a base HREF and resolve resources
+const baseURL = _.trimEnd(_.get(configuration, 'url.base', ''), '/');
+const resources = discovery(baseURL);
 
-    return {
-        authentication: auth,
-        configuration: config,
-        discovery: resources,
-        error: error
-    };
-}();
+module.exports = {
+    authentication: auth,
+    configuration: configuration,
+    discovery: resources,
+    error: error
+};
