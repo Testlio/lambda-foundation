f4ad445a3afbe20ee64de09bca697b85a6491735
Mikk Kirstein <github@kolmas.org>
2016-03-02 17:56:58 +0200

    I did a thing

--- test/model-test.js
+++ test/model-test.js
@@ -3,7 +3,7 @@ const tape = require('tape');
 const Model = require('../lib/model');
 
 /*eslint-disable no-console */
-tape.test('Should create model', function(t) {
+tape.test('should have tableName', function(t) {
     const TestModel = Model('Test', {
         hashKey : 'guid',
         schema : {
@@ -11,36 +11,44 @@ tape.test('Should create model', function(t) {
             property: joi.number()
         }
     });
-    const testModel = new TestModel();
-
-console.log(testModel.get);
     t.equal(TestModel.tableName(), 'test', 'Correct table name');
+    t.end();
+});
 
-    testModel.find('guid').then(function(result) {
+tape.test('should create model', function(t) {
+    const TestModel = Model('Test', {
+        hashKey : 'guid',
+        schema : {
+            guid : joi.string().guid(),
+            property: joi.number()
+        }
+    });
+
+    TestModel.find('guid').then(function(result) {
         console.log('find', result);
     }).catch(function(err)  {
         console.log('find', err);
     });
 
-    testModel.findItems(['guid']).then(function(result) {
+    TestModel.findItems(['guid']).then(function(result) {
         console.log('findItems', result);
     }).catch(function(err)  {
         console.log('findItems', err);
     });
 
-    testModel._query('guid').loadAll().limit(1).exec().then(function(result) {
+    TestModel.query('guid').loadAll().limit(1).exec().then(function(result) {
         console.log('query', result);
     }).catch(function(err)  {
         console.log('query', err);
     });
 
-    testModel.create({guid: '7eacbb91-ef33-4d5a-a10c-2e9747adc2fa', property: 300}).then(function(result) {
+    TestModel.create({guid: '7eacbb91-ef33-4d5a-a10c-2e9747adc2fa', property: 300}).then(function(result) {
         console.log('create', result);
     }).catch(function(err)  {
         console.log('create', err);
     });
 
-    testModel._update({guid: '7eacbb91-ef33-4d5a-a10c-2e9747adc2fa', property: 600}).then(function(result) {
+    TestModel.update({guid: '7eacbb91-ef33-4d5a-a10c-2e9747adc2fa', property: 600}).then(function(result) {
         console.log('update', result);
     }).catch(function(err)  {
         console.log('update', err);
