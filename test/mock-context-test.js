629b11f0ba94ea2f27502e3813a7fac570147438
Kairi Schotter <kairi@teslio.com>
2016-03-26 16:57:02 +0200

    Remove unnecessary tests

--- test/mock-context-test.js
+++ test/mock-context-test.js
@@ -24,20 +24,6 @@ tape.test('Should succeed with callback provided', function(t) {
     mock.succeed({result:"result"});
 });
 
-tape.test('Should fail if fail or done called', function(t) {
-    t.plan(2);
-    const mock = Context.assertSucceed(t, {result: "result"});
-    mock.fail = function(err) {
-        t.ok(err, 'Fail called');
-    };
-    mock.done = function(obj) {
-        t.ok(obj, 'Done called');
-    };
-    mock.fail(new Error('999', 'Unexpected error'));
-    mock.done({result: "unexpected result"});
-});
-
-
 tape.test('Should fail with expected error provided', function(t) {
     const mock = Context.assertFail(t, new Error('999', 'Expected error'));
     t.plan(2);
@@ -60,19 +46,6 @@ tape.test('Should fail with expected error and callback provided', function(t) {
     mock.fail(new Error('999', 'Expected error'));
 });
 
-tape.test('Should fail if succeed or done called', function(t) {
-    t.plan(2);
-    const mock = Context.assertFail(t, new Error('999', 'Expected error'));
-    mock.succeed = function(obj) {
-        t.ok(obj, 'Succeed called');
-    };
-    mock.done = function(obj) {
-        t.ok(obj, 'Done called');
-    };
-    mock.succeed({result: "unexpected result"});
-    mock.done({result: "unexpected result"});
-});
-
 tape.test('Should call done with expected result provided', function(t) {
     const mock = Context.assertDone(t, {result:"result"});
     t.plan(2);
@@ -93,17 +66,4 @@ tape.test('Should call done with callback provided', function(t) {
         t.same(obj, {result:"result"}, 'callback called');
     });
     mock.done({result:"result"});
-});
-
-tape.test('Should fail if fail or succeed called', function(t) {
-    t.plan(2);
-    const mock = Context.assertDone(t, {result: "result"});
-    mock.fail = function(err) {
-        t.ok(err, 'Fail called');
-    };
-    mock.done = function(obj) {
-        t.ok(obj, 'Done called');
-    };
-    mock.fail(new Error('999', 'Expected error'));
-    mock.done({result: "unexpected result"});
 });
