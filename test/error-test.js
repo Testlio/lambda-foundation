14fbf7d0a3cce1664a893b3b0372f78d3735310b
Henri Normak <henri.normak@gmail.com>
2016-06-01 10:17:55 +0300

    Allow reporting extra data along with the error
    This should help with passing along information about the request, context and event to the error report.

--- test/error-test.js
+++ test/error-test.js
@@ -3,6 +3,7 @@
 const tape = require('tape');
 const LambdaError = require('../lib/error');
 const Raygun = require('../lib/error/raygun.js');
+const _ = require('lodash');
 
 // Add a TEST tag to our errors
 LambdaError.config({
@@ -33,12 +34,35 @@ tape.test('Error sent to Raygun', function(t) {
 
     Raygun.send = function(actualError, actualExtra, cb, actualRequest) {
         t.same(actualError, expectedError, 'Error reported to Raygun');
-        t.equal(actualExtra, expectedExtra, 'Extras reported to Raygun');
-        t.equal(actualRequest, expectedRequest, 'Request reported to Raygun');
+        t.same(actualExtra, expectedExtra, 'Extras reported to Raygun');
+        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
         cb();
     };
 
     LambdaError.report(error).then(function() {
+        t.end();
+    });
+});
+
+tape.test('Error sent to Raygun with additional metadata', function(t) {
+    const expectedCode = 401;
+    const expectedMessage = 'A serious error happened';
+    const expectedExtra = {'id': 123};
+    const expectedRequest = {'request':'caused error'};
+    const expectedAdditionalExtra = {'more': 'foo'};
+
+    // The error that is sent through is a native one
+    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
+    const error = new LambdaError(expectedCode, expectedMessage, expectedExtra, expectedRequest);
+
+    Raygun.send = function(actualError, actualExtra, cb, actualRequest) {
+        t.same(actualError, expectedError, 'Error reported to Raygun');
+        t.same(actualExtra, _.merge(expectedAdditionalExtra, expectedExtra), 'Extras reported to Raygun');
+        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
+        cb();
+    };
+
+    LambdaError.report(error, expectedAdditionalExtra).then(function() {
         t.end();
     });
 });
