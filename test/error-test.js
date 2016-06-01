65a79689362d6a600656723283b1a3374dcdd0ca
Henri Normak <henri.normak@gmail.com>
2016-06-01 10:29:04 +0300

    Add a test to check tag sending

--- test/error-test.js
+++ test/error-test.js
@@ -67,6 +67,30 @@ tape.test('Error sent to Raygun with additional metadata', function(t) {
     });
 });
 
+tape.test('Error sent to Raygun with additional tags', function(t) {
+    const expectedCode = 401;
+    const expectedMessage = 'A serious error happened';
+    const expectedExtra = {'id': 123};
+    const expectedRequest = {'request':'caused error'};
+    const expectedAdditionalTags = ['foo', 'bar'];
+
+    // The error that is sent through is a native one
+    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
+    const error = new LambdaError(expectedCode, expectedMessage, expectedExtra, expectedRequest);
+
+    Raygun.send = function(actualError, actualExtra, cb, actualRequest, actualTags) {
+        t.same(actualError, expectedError, 'Error reported to Raygun');
+        t.same(actualExtra, expectedExtra, 'Extras reported to Raygun');
+        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
+        t.same(actualTags, ['TEST'].concat(expectedAdditionalTags));
+        cb();
+    };
+
+    LambdaError.report(error, expectedAdditionalTags).then(function() {
+        t.end();
+    });
+});
+
 tape.test('Error reporting failure resolves to error', function(t) {
     const expectedCode = 401;
     const expectedMessage = 'A serious error happened';
