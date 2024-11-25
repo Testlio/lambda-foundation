2b3b89aca1440e8ec161a0ee9dd71d4ab2b0b682
hayk-simonyan <hayksimonyan7@gmail.com>
2024-11-25 16:00:36 +0400

    chore: remove RayGun integration from the codebase

--- test/error-test.js
+++ test/error-test.js
@@ -2,7 +2,6 @@
 
 const tape = require('tape');
 const LambdaError = require('../lib/error');
-const Raygun = require('../lib/error/raygun.js');
 const _ = require('lodash');
 
 // Add a TEST tag to our errors
@@ -22,88 +21,35 @@ tape.test('Error reporting resolves to error', function(t) {
     });
 });
 
-tape.test('Error sent to Raygun', function(t) {
-    const expectedCode = 401;
-    const expectedMessage = 'A serious error happened';
-    const expectedExtra = {'id': 123};
-    const expectedRequest = {'request':'caused error'};
-
-    // The error that is sent through is a native one
-    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
-    const error = new LambdaError(expectedCode, expectedMessage, expectedExtra, expectedRequest);
-
-    Raygun.send = function(actualError, actualExtra, cb, actualRequest, actualTags) {
-        t.same(actualError, expectedError, 'Error reported to Raygun');
-        t.same(actualExtra, expectedExtra, 'Extras reported to Raygun');
-        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
-        t.same(actualTags, ['TEST'], 'Tags reported to Raygun');
-        cb();
-    };
-
-    LambdaError.report(error).then(function() {
-        t.end();
-    });
-});
-
-tape.test('Error sent to Raygun with additional metadata', function(t) {
+tape.test('Error reporting with additional metadata', function(t) {
     const expectedCode = 401;
     const expectedMessage = 'A serious error happened';
     const expectedExtra = {'id': 123};
     const expectedRequest = {'request':'caused error'};
     const expectedAdditionalExtra = {'more': 'foo'};
 
-    // The error that is sent through is a native one
-    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
     const error = new LambdaError(expectedCode, expectedMessage, expectedExtra, expectedRequest);
 
-    Raygun.send = function(actualError, actualExtra, cb, actualRequest, actualTags) {
-        t.same(actualError, expectedError, 'Error reported to Raygun');
-        t.same(actualExtra, _.merge(expectedAdditionalExtra, expectedExtra), 'Extras reported to Raygun');
-        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
-        t.same(actualTags, ['TEST'], 'Tags reported to Raygun');
-        cb();
-    };
-
-    LambdaError.report(error, undefined, expectedAdditionalExtra).then(function() {
+    LambdaError.report(error, undefined, expectedAdditionalExtra).then(function(err) {
+        t.same(err.extra, _.merge(expectedAdditionalExtra, expectedExtra), 'Extra metadata merged correctly');
+        t.same(err.request, expectedRequest, 'Request data preserved');
         t.end();
     });
 });
 
-tape.test('Error sent to Raygun with additional tags', function(t) {
+tape.test('Error reporting with additional tags', function(t) {
     const expectedCode = 401;
     const expectedMessage = 'A serious error happened';
     const expectedExtra = {'id': 123};
     const expectedRequest = {'request':'caused error'};
     const expectedAdditionalTags = ['foo', 'bar'];
 
-    // The error that is sent through is a native one
-    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
     const error = new LambdaError(expectedCode, expectedMessage, expectedExtra, expectedRequest);
 
-    Raygun.send = function(actualError, actualExtra, cb, actualRequest, actualTags) {
-        t.same(actualError, expectedError, 'Error reported to Raygun');
-        t.same(actualExtra, expectedExtra, 'Extras reported to Raygun');
-        t.same(actualRequest, expectedRequest, 'Request reported to Raygun');
-        t.same(actualTags, ['TEST'].concat(expectedAdditionalTags), 'Tags reported to Raygun');
-        cb();
-    };
-
-    LambdaError.report(error, expectedAdditionalTags).then(function() {
-        t.end();
-    });
-});
-
-tape.test('Error reporting failure resolves to error', function(t) {
-    const expectedCode = 401;
-    const expectedMessage = 'A serious error happened';
-
-    // The error that is sent through is a native one
-    const expectedError = new Error(expectedCode + ': ' + expectedMessage);
-
-    Raygun.send = {};
-
-    LambdaError.report(expectedError).then(function(actualError) {
-        t.same(actualError, expectedError, 'Error reported to Raygun');
+    LambdaError.report(error, expectedAdditionalTags).then(function(err) {
+        t.same(err.extra, expectedExtra, 'Extra metadata preserved');
+        t.same(err.request, expectedRequest, 'Request data preserved');
+        t.same(err.tags, ['TEST'].concat(expectedAdditionalTags), 'Tags merged correctly');
         t.end();
     });
 });
